395bc7d8768216f1bc003b5373339e65c1d0a420
Ciera Hernandez <48962519+armelwl1951@users.noreply.github.com>
2020-08-19 09:57:29 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const apiKey="api_key=JVtf3UDIYsedb3aRSbVFMARUZuhIG7il529eSO34";
+const searchUrl="https://api.si.edu/openaccess/api/v1.0/category/:cat/search"; "https://api.si.edu/openaccess/api/v1.0/search"; "https://api.si.edu/openaccess/api/v1.0/terms/:category";
+
 // header scrolling effect
 $(window).on('scroll', function(){
 	if($(window).scrollTop()){
@@ -10,13 +13,13 @@ $(window).on('scroll', function(){
 	   
 })
 
-//hamburger
+//hamburgler
 const navSlide = () => {
-	 const hamburger = document.querySelector(".hamburger");
+	 const hamburgler = document.querySelector(".hamburgler");
 	 const navbar = document.querySelector(".nav-bar");
 	 const navLinks = document.querySelectorAll(".nav-bar li");
 
-     hamburger.onclick = () => {
+     hamburgler.onclick = () => {
 		
 	 navbar.classList.toggle("nav-active");
 		 
@@ -28,10 +31,67 @@ const navSlide = () => {
 			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
 		   }
 		});
-	  //hamburger animation
-	 hamburger.classList.toggle("toggle");
+	  //hamburgler animation
+	 hamburgler.classList.toggle("toggle");
     }
-	 
-	}
+
+    }
 
 window.onload = () => navSlide();
+
+//search wizard
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    for(let i=0; i<responseJson.data.length; i++) {
+        $('#results-list').append(
+            `<hr><li>
+            <h3>${responseJson.data[i].fullName}</h3>
+            <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
+            <p><b>Description:</b>${responseJson.data[i].description}</p>
+            </li>
+            `
+        )};
+    $('#results').removeClass('hidden');
+}
+
+function getSmithsonianResults(query) {
+    const params={
+        searchCode: query
+    };
+
+    const queryString = formatQueryParams( params );
+    const url =  `${searchURL}?${queryString}&${apiKey}`;
+
+    fetch(url)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            $('#results-list').empty();
+            throw new Error('Nothing found.  Please check spelling and try again.');
+        })
+        .then(data => {
+            console.log(data)
+
+            $('#results-list').empty();
+            displayResults(data, maxResults);
+            $('error-message').empty();
+        })
+        .catch(error => {
+            $('#js-error-message').text(`Something happened: ${error.message}`);
+            $('#results-list').empty();
+        });
+}
+
+function watchForm() {
+    $('form').submit(event=> {
+        console.log('running watchForm')
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+        const maxResults = $('#js-max-results').val();
+        getSmithsonianResults(searchTerm);
+    });
+}
+
+$(watchForm);
