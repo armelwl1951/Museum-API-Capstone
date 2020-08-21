911d9bd276df64ebdd0948f3de8873f58515cb62
Ciera Hernandez <ciera.hernandez@outlook.com>
2020-08-21 23:43:13 +0000

    Getting response from API, nothing being displayed

--- scripts.js
+++ scripts.js
@@ -4,57 +4,38 @@
 const apiKey = "JVtf3UDIYsedb3aRSbVFMARUZuhIG7il529eSO34";
 const searchURL = "https://api.si.edu/openaccess/api/v1.0/search";
 
-function formatQueryParams( params ) {
-  const queryItems = Object.keys( params )
-    .map( key => `${encodeURIComponent( key )}=${encodeURIComponent( params[ key] )}` )
-  return queryItems.join( '&' );
-}
-
-function displayResults( responseJson ) {
-  console.log( responseJson );
-  for ( let i = 0; i < responseJson.data.length; i++ ) {
-    $( '#results-list' ).append(
-      ` <hr><li>
-          <h3>${responseJson.data[i].fullName}</h3>
-          <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
-          <p><b>Address:</b> ${responseJson.data[i].addresses[0].line1} ${responseJson.data[i].addresses[0].line2} ${responseJson.data[i].addresses[0].line3}  ${responseJson.data[i].states}, ${responseJson.data[i].addresses[0].postalCode}</p>
-          <p><b>Directions:</b> ${responseJson.data[i].directionsInfo}</p>
-          <p><b>Description:</b> ${responseJson.data[i].description}</p>
-        </li>
-      `
-    )};
-  $( '#results' ).removeClass( 'hidden' );
-}
-
 function getResults( query, maxResults ) {
   const params = {
     userInput: query,
     limit: maxResults
   };
 
-  const queryString = formatQueryParams( params );
-  const url = `${searchURL}?q=${queryString}&api_key=${apiKey}`;
+  const url = `${searchURL}?q=${query}&api_key=${apiKey}`;
 
   fetch(url)
     .then( response => {
       if ( response.ok ) {
         return response.json();
       }
-      $( '#results-list' ).empty();
-      throw new Error( 'Query not found.  Please try again.' );
-    })
-    .then( data => {
-      console.log(data)
-
-      $( '#results-list' ).empty();
-      displayResults( data, maxResults );
-      $( '.error-message' ).empty();
+      throw new Error("We couldn't find anything.  Please check your spelling and try again.");
+      })
+    .then( responseJson => {
+      $('.error').empty();
+      displayResults(responseJson);
     })
     .catch( error => {
       $( '#js-error-message' ).text( `Something went wrong:  ${error.message}` );
       $( '#results-list' ).empty();
     });
 }
+
+function displayResults( responseJson ) {
+  console.log( responseJson );
+  $('#results-list').empty();
+  $('.restuls-img').html(`<img src="${response.message}" class="results-img">`);
+  $('.results').removeClass('hidden');
+}
+
 
 function watchForm() {
   $( 'form' ).submit( event => {
@@ -66,7 +47,10 @@ function watchForm() {
   });
 }
 
-$( watchForm );
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
 
 // header scrolling effect
 $(window).on('scroll', function(){
